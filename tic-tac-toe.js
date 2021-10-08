bf942da40ef08f9e8f743a0b076d151b090d7efe
MiDevenKnow <kabiandavidson19@gmail.com>
2021-10-07 22:36:36 -0500

    x & O completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    var board = [];
 
     //Get all divs after an element with id "board"
     let squares = document.querySelectorAll("#board div");
@@ -7,5 +8,23 @@ window.onload = function(){
     for(let x = 0; x <= 8; x++){
         squares[x].setAttribute("class", "square")
     }
+    
+    var num_clicks = 9;
+    for(let x = 0; x <= 8; x++){
+        squares[x].addEventListener('click', function(){
+            if(num_clicks%2==0 && squares[x].innerHTML==""){
+                squares[x].setAttribute("class","square X");
+                squares[x].innerHTML="X";
+                num_clicks--;
+            }
+            if(num_clicks%2==1 && squares[x].innerHTML==""){
+                squares[x].setAttribute("class","square O");
+                squares[x].innerHTML="O";
+                num_clicks--;
+            }
+        });
+    }
+
+
 
 }
